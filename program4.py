532bbf26ea9b2e42d04deb91b437a8219707dd75
dheeproject <62594025+dheeproject@users.noreply.github.com>
2020-05-27 11:21:29 -0700

    Delete program4.py

--- program4.py
+++
@@ -1,6 +0,0 @@
-cp=float(input("Cost price of prodoct"))
-sp=float(input("Selling price of product"))
-oprofit=sp-cp
-print("profit with this sp is=",oprofit)
-newsp=cp+oprofit+(5/100)*cp;
-print("newsp=",newsp)
