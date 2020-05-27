74a1f21e6d1b765d5310efbaf3038edaa690f004
dheeproject <62594025+dheeproject@users.noreply.github.com>
2020-05-27 11:21:52 -0700

    Add files via upload

---
+++ Day1/program4.py
@@ -0,0 +1,6 @@
+cp=float(input("Cost price of prodoct"))
+sp=float(input("Selling price of product"))
+oprofit=sp-cp
+print("profit with this sp is=",oprofit)
+newsp=cp+oprofit+(5/100)*cp;
+print("newsp=",newsp)
